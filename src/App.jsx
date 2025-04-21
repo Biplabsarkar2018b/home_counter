f3b105b7bdecc0859c1e291a93810e51ca96e3e5
Biplab Sarkar <94435217+Biplabsarkar2018b@users.noreply.github.com>
2025-04-22 00:24:07 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
   useEffect(() => {
     const calculateTimeLeft = () => {
-      const arrivalDate = new Date('2025-02-04');
+      const arrivalDate = new Date('2025-09-21');
       const now = new Date();
       const difference = arrivalDate - now;
 
